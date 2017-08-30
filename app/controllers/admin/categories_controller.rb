0ad6037670f5f2ca85306e44c5771b70de96b900
Jonathan Sue <hello@jonosue.com>
2017-08-30 22:30:29 +0000

    Added methods to handle index, create, and new Categories.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -17,4 +17,12 @@ class Admin::CategoriesController < ApplicationController
     end
   end
 
+  private
+
+    def category_params
+      params.require(:category).permit(
+        :name
+      )
+    end
+
 end
