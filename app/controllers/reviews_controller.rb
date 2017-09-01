d30e9a12e23ba2f8445656cfbd356467780abf65
Jonathan Sue <hello@jonosue.com>
2017-09-01 07:23:51 +0000

    Added functionality for deleting reviews, which requiring defining new routes.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,13 @@
 class ReviewsController < ApplicationController
-  before_action :deny_access, :unless => current_user
+  before_action :delete_review, :only => [:destroy]
+  before_action :new_review, :only => [:create]
+
+  def destroy
+    @delete.destroy
+    redirect_to '/'
+  end
+
   def create
-    @review = Review.new(review_params)
     @review.user = current_user
     @review.product_id = params[:product_id]
     if @review.save
@@ -11,9 +17,18 @@ class ReviewsController < ApplicationController
     end
   end
 
- private
-  def review_params
-    params.require(:review).permit(:description, :rating)
-  end
+  private
+    def review_params
+      params.require(:review).permit(:description, :rating)
+    end
+
+  protected
+    def delete_review
+      @delete = Review.find params[:id]
+    end
+
+    def new_review
+      @review = Review.new(review_params)
+    end
 
 end
