37b1203c962091e7da0a1943f5041b0f8e02a5fd
Jonathan Sue <hello@jonosue.com>
2017-09-01 05:55:45 +0000

    Fixed the form for the product review so it works properly. Added requirements so that users are forced to make selections for both form fields to make a review.

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @review.product_id = params[:product_id]
+    if @review.save
+      redirect_to '/'
+    else
+      redirect_to :back
+    end
+  end
+
+ private
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
+
+end
