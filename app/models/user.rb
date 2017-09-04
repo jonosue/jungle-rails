faed8e75cfe1b785eb4bd1df416d7765fe391e18
Jonathan Sue <hello@jonosue.com>
2017-09-04 21:39:27 +0000

    Defined a new class method and added validation checks for the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6 }
+
+  def self.authenticate_with_credentials(email, password)
+    @user = self.find_by_email(email.delete(' ')).try(:authenticate, password)
+  end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    it 'should create a User if all of the validations are true' do
+    @user = User.new(first_name: "John", last_name: "Smith", email: "johnsmith@email.com", password: "ABC", password_confirmation: "ABC")
+    @user.valid?
+    expect(@user.errors).not_to include("can\'t be blank")
+    end
+    it 'should not create a User if their last name is missing' do
+    @user = User.new(first_name: "John", email: "johnsmith@email.com", password: "ABCDEF", password_confirmation: "ABCDEF")
+    @user.valid?
+    expect(@user.errors[:last_name]).to include("can\'t be blank")
+    end
+    it 'should not create a User if their first name is missing' do
+    @user = User.new(last_name: "Smith", email: "johnsmith@email.com", password: "ABCDEF", password_confirmation: "ABCDEF")
+    @user.valid?
+    expect(@user.errors[:first_name]).to include("can\'t be blank")
+    end
+    it 'should not create a User if their email is missing' do
+    @user = User.new(last_name: "Smith", first_name: "John", password: "ABCDEF", password_confirmation: "ABCDEF")
+    @user.valid?
+    expect(@user.errors[:email]).to include("can\'t be blank")
+    end
+    it 'should not create a User if their email is not unique' do
+    @user1 = User.new(first_name: "John", last_name: "Smith", email: "john@gmail.com", password: "ABCDEF", password_confirmation: "ABCDEF")
+    @user1.save
+    @user2 = User.new(first_name: "Bill", last_name: "Ericsson", email: "JOHN@gmail.com", password: "ABCDEF", password_confirmation: "ABCDEF")
+    @user2.valid?
+    expect(@user2.errors[:email]).to include("has already been taken")
+    end
+    it 'should not create a User if their passwords do not match' do
+    @user = User.new(first_name: "John", last_name: "Smith", email: "john@gmail.com", password: "ABCDEF", password_confirmation: "ABCDEFG")
+    @user.valid?
+    expect(@user.errors[:password_confirmation]).to include("doesn\'t match Password")
+    end
+    it 'should not create a User if there is no password' do
+    @user = User.new(first_name: "John", last_name: "Smith", email: "john@gmail.com")
+    @user.valid?
+    expect(@user.errors[:password_digest]).to include("can\'t be blank")
+    end
+    it 'should not create a User if there the password is too short' do
+    @user = User.new(first_name: "John", last_name: "Smith", email: "john@gmail.com", password: "ABC", password_confirmation: "ABC")
+    @user.valid?
+    expect(@user.errors[:password]).to include("is too short (minimum is 6 characters)")
+    end
+  end
+end
